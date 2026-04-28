5a53799e37e93c44137cc821124f4ab222ef9e13
dantech13 <daniil.mur2004@gmail.com>
2026-04-29 01:15:44 +0200

    Rozbudowanie klasy CorporateWellnessPlan wraz z logiką biznesową

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ public class Main {
                 new PersonalTrainingPlan("PT-201", "Jan Malinowski", 6, 150.0, false, 8, 3, true),
                 new OnlineCoachingPlan("ONL-300", "Karolina Wisniewska", 6, 89.0, true, 2, true, true),
                 new OnlineCoachingPlan("ONL-301", "Pawel Adamski", 3, 79.0, false, 1, false, true),
-                //new CorporateWellnessPlan("CORP-400", "TechNova Sp. z o.o.", 12, 350.0, true, 25, 2, true),
-               // new CorporateWellnessPlan("CORP-401", "SoftLine S.A.", 6, 320.0, false, 14, 0, false)
+                new CorporateWellnessPlan("CORP-400", "TechNova Sp. z o.o.", 12, 350.0, true, 25, 2, true),
+                new CorporateWellnessPlan("CORP-401", "SoftLine S.A.", 6, 320.0, false, 14, 0, false)
         };
 
         double totalMonthlyNet = 0.0;
